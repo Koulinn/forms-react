a7a12037a5c059c5e15c3d1c62e6009008cbd8a2
Rafael Lima <drdverzola@gmail.com>
2022-04-03 11:55:42 +0100

    Form with dynamic property

--- src/component/MyForm.jsx
+++ src/component/MyForm.jsx
@@ -2,70 +2,40 @@ import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 
 function MyForm() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [select, setSelect] = useState("");
-  const [radio, setRadio] = useState(false);
-  const [flag, setFlag] = useState(false);
-
-  useEffect(() => {
-    handleSubmit();
-  }, [email, name, select, radio, flag]);
-
-  //I want the preventDefault to be invoked ONLY when e is defined
-
-  /**
-   * Our post request expects
-   * primaryEmail: ''Needs an at @ String
-   * first_name: '' String
-   * category:'' String
-   * sex: ''String
-   * newsletter: Boolean
-   *
-   *
-   */
-
-  //You will ALWAYS RECEIVE at least name + surname
-  // Valid Rafael Lima
-  // Valid Rafael Verzola Peres de Lima
-  // Valid Rafael V P Lima
-  // Invalid Rafael
-  // Invalid ''
-
-  // Where : inside handleSubmit
-  // When : Before you define the body
-  // What :
-  // How :
-
-  /**
-   * first_name : Expects to receive ONLY the first name (even if it is a composite name)
-   * surname : Expects to receive at least a surname (can receive middle names)
-   */
+  const [form, setForm] = useState({
+    email: "",
+    name: "",
+    select: "",
+    radio: "",
+    flag: false,
+  });
 
   const handleSubmit = (e) => {
     if (e !== undefined) {
       e.preventDefault();
     }
+
+    // console.log(form, "====form====");
     // It needs to happen between this lines======================
-    const array = name.split(" ");
+    const array = form.name.split(" ");
 
     const firstName = array[0];
     const lastname = array.slice(1).join(" ");
     // ===========================================================
     // If you need to prepare a body
     const body = {
-      primaryEmail: email,
+      primaryEmail: form.email,
       first_name: firstName,
       surname: lastname,
-      category: select,
-      sex: radio,
-      newsletter: flag,
+      category: form.select,
+      sex: form.radio,
+      newsletter: form.flag,
     };
-    //
-    // console.log(body, "body");
-    //Send a request to the server
-    // postRequest(body)
-    // we need to prepare the body!!!!!!!!!!!!!!
+    console.log(body, "=== body ===");
+  };
+
+  const handleForm = (value, dynamicProperty) => {
+    setForm({ ...form, [dynamicProperty]: value });
   };
 
   return (
@@ -76,14 +46,12 @@ function MyForm() {
           <Form.Label>Email address</Form.Label>
           <Form.Control
             type="email"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+            onChange={(e) => handleForm(e.target.value, "email")}
             placeholder="Enter email"
           />
         </Form.Group>
         <div>
-          <h5>Email state: {email}</h5>
+          <h5>Email state: {form.email}</h5>
         </div>
       </div>
 
@@ -92,12 +60,12 @@ function MyForm() {
           <Form.Label>Full name</Form.Label>
           <Form.Control
             type="text"
-            onChange={(e) => setName(e.target.value)}
+            onChange={(e) => handleForm(e.target.value, "name")}
             placeholder="Name"
           />
         </Form.Group>
         <div>
-          <h5>name state: {name}</h5>
+          <h5>name state: {form.name}</h5>
         </div>
       </div>
 
@@ -106,9 +74,7 @@ function MyForm() {
       <Form.Select
         aria-label="Default select example"
         className="my-5"
-        onChange={(e) => {
-          setSelect(e.target.value);
-        }}
+        onChange={(e) => handleForm(e.target.value, "select")}
       >
         <option>Open this select menu</option>
         <option value="1">One</option>
@@ -124,32 +90,29 @@ function MyForm() {
           id={`default-${"checkbox"}`}
           label={`Would you like to receive offers?`}
           // onChange={(e) => {
-          //   console.log(e.target.checked);
+          //   e.target.checked ? setFlag(true) : setFlag(false);
           // }}
-          onChange={(e) => {
-            e.target.checked ? setFlag(true) : setFlag(false);
-            console.log(e.target.checked);
-          }}
+          onChange={(e) =>
+            e.target.checked
+              ? handleForm(true, "flag")
+              : handleForm(false, "flag")
+          }
           value="I'm a checkbox with a value!!!!!"
         />
         <Form.Check
           type={"radio"}
           id={`default-${"radio"}`}
           label={`female`}
-          onChange={(e) => {
-            setRadio(e.target.value);
-          }}
-          checked={radio === "female"}
+          onChange={(e) => handleForm(e.target.value, "radio")}
+          checked={form["radio"] === "female"}
           value="female"
         />
         <Form.Check
           type={"radio"}
           id={`default-${"radio1"}`}
           label={`male`}
-          onChange={(e) => {
-            setRadio(e.target.value);
-          }}
-          checked={radio === "male"}
+          onChange={(e) => handleForm(e.target.value, "radio")}
+          checked={form.radio === "male"}
           value="male"
         />
       </div>
